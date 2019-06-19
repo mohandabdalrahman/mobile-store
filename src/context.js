763de113909ae05b743370e2ab4e0dc7070655dd
Mohand Abdalrahman <mohand.abdalrahman@mondiamedia.com>
2019-06-19 11:11:23 +0200

    details page

--- src/context.js
+++ src/context.js
@@ -1,36 +1,52 @@
-import React, { Component } from 'react';
-import {storeProducts,detailProduct} from './data'
-const ProductContext=React.createContext();
+import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
+const ProductContext = React.createContext();
 // it give us 2 components
 // Provider component => to provide data to all applications
 // Consumer component => consume data from every where in application
 export default class ProductProvider extends Component {
-    state={
-        products: storeProducts,
-        detailProduct
-    }
+  state = {
+    products: [],
+    detailProduct
+  };
 
-    handleDetail=()=>{
-        console.log('hello from detail')
-    }
-    addToCart=()=>{
-console.log('hello from add to cart')
-    }
-    render() {
-        return (
-            <ProductContext.Provider value={{
-                ...this.state,
-                handleDetail:this.handleDetail,
-                addToCart:this.addToCart
-                }}>
-                {this.props.children}
-            </ProductContext.Provider>
-        )
-    }
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return {
+        products: tempProducts
+      };
+    });
+  };
+
+  handleDetail = () => {
+    console.log("hello from detail");
+  };
+  addToCart = () => {
+    console.log("hello from add to cart");
+  };
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 const ProductConsumer = ProductContext.Consumer;
 
-export {
-    ProductProvider,
-    ProductConsumer
-}
+export { ProductProvider, ProductConsumer };
